df95e006f9a72f3b3c93bb3db8e86f4a21489c02
studioo <studioo@example.org>
2018-11-26 12:47:56 +0100

    Update highlight block: add respons.

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -7,9 +7,11 @@ const Highlight = (props: HighlightProps) => {
     <div className="fullWidthContainer">
       <section className={'highlight'}>
         <div className="container">
-          <div className="d-flex">
-            <img src="assets/medicon/images/info.png" alt="info" />
-            <p>Poliklinika Zelený pruh je od 16.9. do 19.9. zavřená</p>
+          <div className="flexRow flexAlign--center">
+            <div className="info flexRow">
+              <img src="assets/medicon/images/info.png" alt="info" />
+              <p>Poliklinika Zelený pruh je od 16.9. do 19.9. zavřená</p>  
+            </div>
             <button className="btn btnSecond">vice info</button>
           </div>
         </div>
