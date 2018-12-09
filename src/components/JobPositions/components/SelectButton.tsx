8d989432995bf28388adb5d0a364469f18b4156c
gules <s@m.com>
2018-12-09 14:39:12 +0100

    Update JobPositions component: fix hardcoded names in SelectButton comp

--- src/components/JobPositions/components/SelectButton.tsx
+++ src/components/JobPositions/components/SelectButton.tsx
@@ -6,9 +6,9 @@ const SelectButton = (props: SelectButtonProps) => (
   <div className={'select-btn hCenterBlock'}>
     <select>
       <option value="Všechny polikliniky">Všechny polikliniky</option>
-      <option value="saab">Saab</option>
-      <option value="mercedes">Mercedes</option>
-      <option value="audi">Audi</option>
+      <option value="saab">Poliklinika 1</option>
+      <option value="mercedes">Poliklinika 2</option>
+      <option value="audi">Poliklinika 3</option>
     </select>
     
     <span className="arrow" />
