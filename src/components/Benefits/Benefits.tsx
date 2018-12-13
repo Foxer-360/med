6ceef52b45b53379691abb391023e515e2dc2b3f
gules <s@m.com>
2018-12-13 23:38:24 +0100

    Update Benefits component

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,31 +1,35 @@
 import * as React from 'react';
 import Media from '@source/partials/Media';
 
+interface Benefits {
+  text: string;
+  image: LooseObject;
+}
+
 export interface BenefitsProps { 
   data: {
-    items: [
-      {
-        text: string;
-        image: object;
-      }
-    ];
+    benefits: Benefits[];
   };
 }
 
 const Benefits = (props: BenefitsProps) => {
-  const { items } = props.data;
+  const { benefits } = props.data;
 
   return (
     <section className={'benefits'}>
       <div className={'container'}>
         <div className={'grid benefits__list'}>
-          
-          {/* /assets/medicon/images/benefity1.png */}
-          {items && items.map((item, index) => {
+
+          {benefits && benefits.map((benefit, index) => {
             return (
               <div key={index} className={'grid benefits__list__element'}>
-                <Media type={'image'} data={item.image} />
-                <p>{item.text}</p>
+                {(benefit.image && benefit.image.filename && <Media type={'image'} data={benefit.image} />) || (
+                  <img 
+                    src={'/assets/medicon/images/benefity1.png'}
+                    alt="Our benefit"
+                  />
+                )}
+                <p>{benefit.text}</p>
               </div>
             );
           })}
