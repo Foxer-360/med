f41502eb206e268ff8a9f59b781c75382cea4b89
gules <s@m.com>
2018-12-15 19:56:36 +0100

    Update InfoRectangles component: add form to backoffice

--- src/components/InfoRectangles/InfoRectangles.tsx
+++ src/components/InfoRectangles/InfoRectangles.tsx
@@ -1,26 +1,37 @@
 import * as React from 'react';
 import Button from '../../partials/Button';
+import Media from '@source/partials/Media';
 
-export interface InfoRectanglesProps {}
+interface InfoRectangles {
+  image: LooseObject;
+  title: string;
+  url: string;
+}
+
+export interface InfoRectanglesProps {
+  data: {
+    infoRectangles: InfoRectangles[];
+  };
+}
 
 const InfoRectangles = (props: InfoRectanglesProps) => {
+  const { infoRectangles } = props.data;
+
   return (
     <section className="info-rectangles">
     
       <div className="container">
         <div className="grid-container">
-          <div className="info-element" style={{ backgroundImage: 'url(/assets/medicon/images/rect-1.jpg' }}>
-            <div>
-              <h5>široký výběr zdravýchpotravin</h5>
-              <Button classes="btn--blueBorder">vice info</Button>
-            </div>
-          </div>
-          <div className="info-element">
-            <div style={{ backgroundImage: 'url(/assets/medicon/images/rect-2.jpg' }}>
-              <h5>široký výběr zdravýchpotravin</h5>
-              <Button classes="btn--blueBorder">vice info</Button>
-            </div>
-          </div>
+
+          {infoRectangles && infoRectangles.map((rectangle, index) => (
+            <Media type={'background-image'} classes={'info-element'} data={rectangle.image} key={index}>
+              <div>
+                <h5>{rectangle.title}</h5>
+                <Button classes="btn--blueBorder">vice info</Button>
+              </div>
+            </Media>
+          ))}
+          
         </div>
       </div>
       
