8a13023b8acc4aa001cc22618a6a69f6a7de39da
gules <s@m.com>
2018-12-15 14:45:14 +0100

    Update Review component: add form to backoffice

--- src/components/Reviews/components/ReviewElement.tsx
+++ src/components/Reviews/components/ReviewElement.tsx
@@ -1,24 +1,35 @@
 import * as React from 'react';
 import SvgIcon from '@source/partials/SvgIcon';
+import Media from '@source/partials/Media';
 
 export interface ReviewElementProps {
-  img: string;
   cite: string;
+  image: LooseObject;
   starCount: number;  
 }
 
 export default function ReviewElement(props: ReviewElementProps) {
+  const { image, cite, starCount } = props;
+
   var rows = [];
-  for (var i = 0; i < props.starCount; i++) {
-    rows.push(<SvgIcon name="star" key={i} />);
+  if (starCount > 0) {
+    for (let index = 0; index < starCount; index++) {
+      rows.push(<SvgIcon name="star" key={index} />);
+    }
+  } else {
+    for (let index = 0; index < 5; index++) {
+      rows.push(<SvgIcon name="star" key={index} />);
+    }
   }
+  
+  console.log(`Review :: ` + props);
 
   return (
     <div className="reviews__list__element">
-      <img src={props.img} alt="photo" />
+      <Media type={'image'} data={image}/>
 
       <div className="reviews__list__element__content">
-        <cite>{props.cite}</cite>
+        <cite>{cite}</cite>
         <div className="stars">{rows}</div>
       </div>
     </div>
