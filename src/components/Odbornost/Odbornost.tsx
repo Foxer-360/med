9e0925d62461576279af0a814478fb7bb02f4696
studioo <studioo@example.org>
2018-12-19 11:55:26 +0100

    Update odbornost component: fix variable name

--- src/components/Odbornost/Odbornost.tsx
+++ src/components/Odbornost/Odbornost.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import TextBlock from '../TextBlock';
 import DividerCircles from '../DividerCircles';
 import Media from '@source/partials/Media';
+import getImageUrl from '@source/helpers/getImageUrl';
 
 export interface OdbornostProps {
   data: {
     image: LooseObject;
-    doctor: string;
+    doctorName: string;
     cite: string;
   };
 }
 
 const Odbornost = (props: OdbornostProps) => {
-  const { image, doctor, cite } = props.data;
+  const { image, doctorName, cite } = props.data;
 
   return (
     <div className={'odbornost'}>
@@ -24,10 +24,10 @@ const Odbornost = (props: OdbornostProps) => {
             <img src={'/assets/medicon/images/odbornost-lekar.jpg'} alt="Doctor photo"/>
           )}
 
-          <div className="grid grid--vCenterBlock">
+          <div className={'grid grid--vCenterBlock'}>
             <div>
                 <div className={'horizontal-line'} />
-                {doctor && <p>{doctor}</p>}
+                {doctorName && <p>{doctorName}</p>}
                 {cite && <cite>{cite}</cite>}
               </div>
             </div>
