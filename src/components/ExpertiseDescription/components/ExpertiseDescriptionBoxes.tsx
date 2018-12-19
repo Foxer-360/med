32890ba337204a003418d8c47078bd6deeef175f
studioo <studioo@example.org>
2018-12-19 11:20:33 +0100

    Update ExpertiseDesc component: fix background-images

--- src/components/ExpertiseDescription/components/ExpertiseDescriptionBoxes.tsx
+++ src/components/ExpertiseDescription/components/ExpertiseDescriptionBoxes.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Button from '../../../partials/Button';
+import getImageUrl from '../../../helpers/getImageUrl';
 
 export interface ExpertiseDescriptionBoxesProps {
   boxes: Array<any>;
@@ -16,7 +17,7 @@ const ExpertiseDescriptionBoxes = (props: ExpertiseDescriptionBoxesProps) => {
           <div
             key={i} 
             className={'info__element'} 
-            style={{ backgroundImage: 'url("/assets/medicon/images/rect-1.jpg' }}
+            style={{ backgroundImage: box.image && `url(${getImageUrl(box.image)})` }}
           >
             <div className={'info__element--cell'}>
               {box.title && <h5>{box.title}</h5>}
