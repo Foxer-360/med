eab21c062dd413e93bd8a4f40b0ecbf81fb3941b
Nick K <studioo@mail.ru>
2019-02-14 14:06:16 +0100

    Update highlight component

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -1,27 +1,34 @@
 import * as React from 'react';
 import Button from '../../partials/Button';
+import ReactMarkdown from 'react-markdown';
 
 export interface HighlightProps {
   data: {
     text: string;
+    description: string;
+    urlTitle: string;
     url: LooseObject;
   };
   languageCode?: string;
 }
 
 const Highlight = (props: HighlightProps) => {
-  const { text, url } = props.data;
+  const { text, url, description, urlTitle } = props.data;
 
   return (
     <div className={'highlight'}>
       <div className={'container'}>
         <div className={'highlight__content grid'}>
           <img src="/assets/medicon/images/info.png" alt="info" />
-          {text && <p>{text}</p>}
+          <div>
+            {text && <p>{text}</p>}
+            {description && 
+              <ReactMarkdown className={'highlight__content--description'} source={description} />}
+          </div>
           
-          <Button classes={'btn--whiteBorder'} url={url && url}>
-            vice info
-          </Button>
+          {url && <Button classes={'btn--whiteBorder'} url={url}>
+            {urlTitle && urlTitle || 'more info'}
+          </Button>}
         </div>
       </div>
     </div>
