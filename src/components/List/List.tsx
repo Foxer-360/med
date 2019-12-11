6f9f3794362d4fb64a48d58f60a900774684cf1f
Petr <pesusak@gmai.com>
2019-12-11 13:38:33 +0100

    Adding czech alphabetical sorting to all other places where sorting is used

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -590,14 +590,9 @@ class List extends React.Component<Properties, {}> {
         datasourceItems
           .sort((a, b) => {
             if (data.order === 'DESC') {
-              if (a.orderBy > b.orderBy) { return -1; }
-              { if (a.orderBy < b.orderBy) { return 1; } }
-              return 0;
+              return a.orderBy.localeCompare(b.orderBy, 'cs', { sensitivity: 'base' });
             }
-
-            if (a.orderBy < b.orderBy) { return -1; }
-            { if (a.orderBy > b.orderBy) { return 1; } }
-            return 0;
+            return a.orderBy.localeCompare(b.orderBy, 'cs', { sensitivity: 'base' });
           })
           .map(item => {
             delete item.orderBy;
