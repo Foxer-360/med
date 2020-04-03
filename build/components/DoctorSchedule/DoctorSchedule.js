ca943566e763541097b5481459a5545ed6507314
Petr <pesusak@gmai.com>
2020-04-03 13:52:43 +0200

    build

--- build/components/DoctorSchedule/DoctorSchedule.js
+++ build/components/DoctorSchedule/DoctorSchedule.js
@@ -139,6 +139,11 @@ var getPolyclinicPhone = function (phones, doctor) {
 var futureEmployee = function (date) {
     return moment(date) > moment();
 };
+var hasSchedule = function (schedule) {
+    return schedule.weeks
+        .some(function (week) { return Object.keys(week.days)
+        .some(function (day) { return week.days[day].length > 0; }); });
+};
 var DoctorSchedule = function (props) {
     var _a = props.data, schedule = _a.schedule, oddWeekTitle = _a.oddWeekTitle, evenWeekTitle = _a.evenWeekTitle, regularWeekTitle = _a.regularWeekTitle, absences = _a.absences, extraAbsenceSettings = _a.extraAbsenceSettings, doctor = _a.doctor, defaultAbsenceMessage = _a.defaultAbsenceMessage, doctorName = _a.doctorName, employmentFrom = _a.employmentFrom, phone = _a.phone;
     var absenceMessage = absenceSettings(extraAbsenceSettings, doctor);
@@ -157,7 +162,8 @@ var DoctorSchedule = function (props) {
                     url: null
                 } })
             : '',
-        Array.isArray(absences) && highlightAbsence(defaultAbsenceMessage, absences, absenceMessage),
+        Array.isArray(absences) && hasSchedule(schedule) &&
+            highlightAbsence(defaultAbsenceMessage, absences, absenceMessage),
         schedule &&
             schedule.weeks &&
             schedule.weeks.map(function (week, i) { return (React.createElement("div", { className: "doctorSchedule", key: week.regularity },
@@ -202,27 +208,28 @@ var DoctorSchedule = function (props) {
                         "V urgentn\u00EDch p\u0159\u00EDpadech volejte ",
                         getPolyclinicPhone(phone, week.polyclinic.shortName),
                         "."))); }),
-        React.createElement(react_apollo_1.Query, { query: GET_CONTEXT }, function (_a) {
-            var data = _a.data;
-            var nextMonthAbsences = Array.isArray(absences) && absences.filter(function (absence) {
-                return absence && moment(absence.fromDate.date) < moment().add(1, 'M')
-                    && moment(absence.toDate.date) > moment();
-            });
-            return (React.createElement(React.Fragment, null, nextMonthAbsences && Array.isArray(nextMonthAbsences) && nextMonthAbsences.length > 0 && (React.createElement("div", { className: 'absences' },
-                React.createElement("h4", null, "Nep\u0159\u00EDtomnost"),
-                React.createElement("table", null,
-                    React.createElement("thead", null,
-                        React.createElement("tr", null,
-                            React.createElement("td", null, "Od"),
-                            React.createElement("td", null, "Do"),
-                            React.createElement("td", null, "Zastupuje"))),
-                    React.createElement("tbody", null, nextMonthAbsences.map(function (absence, i) { return (React.createElement("tr", { key: i },
-                        React.createElement("td", null, (absence.fromDate && moment(absence.fromDate.date).format('DD.MM.YYYY')) || ''),
-                        React.createElement("td", null, (absence.toDate.date && absence.subcategory.id !== 31 &&
-                            moment(absence.toDate.date).format('DD.MM.YYYY')) || ''),
-                        React.createElement("td", null, Array.isArray(absenceMessage) ? (React.createElement(ReactMarkdown, { skipHtml: false, escapeHtml: false, source: absenceMessage[2] })) :
-                            React.createElement(Link_1.default, { dynamic: true, url: getAbsenceLink(data, absence.alternate) }, ((absence.alternate && absence.alternate.firstName) || '') + " \n                            " + ((absence.alternate && absence.alternate.lastName) || ''))))); })))))));
-        })));
+        hasSchedule(schedule) &&
+            React.createElement(react_apollo_1.Query, { query: GET_CONTEXT }, function (_a) {
+                var data = _a.data;
+                var nextMonthAbsences = Array.isArray(absences) && absences.filter(function (absence) {
+                    return absence && moment(absence.fromDate.date) < moment().add(1, 'M')
+                        && moment(absence.toDate.date) > moment();
+                });
+                return (React.createElement(React.Fragment, null, nextMonthAbsences && Array.isArray(nextMonthAbsences) && nextMonthAbsences.length > 0 && (React.createElement("div", { className: 'absences' },
+                    React.createElement("h4", null, "Nep\u0159\u00EDtomnost"),
+                    React.createElement("table", null,
+                        React.createElement("thead", null,
+                            React.createElement("tr", null,
+                                React.createElement("td", null, "Od"),
+                                React.createElement("td", null, "Do"),
+                                React.createElement("td", null, "Zastupuje"))),
+                        React.createElement("tbody", null, nextMonthAbsences.map(function (absence, i) { return (React.createElement("tr", { key: i },
+                            React.createElement("td", null, (absence.fromDate && moment(absence.fromDate.date).format('DD.MM.YYYY')) || ''),
+                            React.createElement("td", null, (absence.toDate.date && absence.subcategory.id !== 31 &&
+                                moment(absence.toDate.date).format('DD.MM.YYYY')) || ''),
+                            React.createElement("td", null, Array.isArray(absenceMessage) ? (React.createElement(ReactMarkdown, { skipHtml: false, escapeHtml: false, source: absenceMessage[2] })) :
+                                React.createElement(Link_1.default, { dynamic: true, url: getAbsenceLink(data, absence.alternate) }, ((absence.alternate && absence.alternate.firstName) || '') + " \n                            " + ((absence.alternate && absence.alternate.lastName) || ''))))); })))))));
+            })));
 };
 exports.default = DoctorSchedule;
 var templateObject_1;
