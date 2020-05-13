92883da385a5555850ffe0a8176248aa0cf1595f
Petr <pesusak@gmai.com>
2020-05-13 14:53:38 +0200

    build

---
+++ build/components/DoctorDetails/DoctorDetails.js
@@ -0,0 +1,118 @@
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactMarkdown = require("react-markdown/with-html");
+var graphql_tag_1 = require("graphql-tag");
+var react_apollo_1 = require("react-apollo");
+var Link_1 = require("../../partials/Link");
+var Media_1 = require("../../partials/Media");
+var GET_CONTEXT = graphql_tag_1.default(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  {\n    languageData @client\n    pageData @client\n    websiteData @client\n    languagesData @client\n    navigationsData @client\n  }\n"], ["\n  {\n    languageData @client\n    pageData @client\n    websiteData @client\n    languagesData @client\n    navigationsData @client\n  }\n"])));
+var getContactCenterPhone = function (clinic) {
+    var shortName = clinic && clinic.shortName && clinic.shortName.split(',')[0];
+    switch (shortName) {
+        case 'BUD':
+            return '237 777 200';
+        case 'VYS':
+            return '266 006 211';
+        case 'HOL':
+            return '227 777 677';
+        case 'ZP':
+            return '234 105 402';
+        default:
+            // tslint:disable-next-line: no-unused-expression
+            '';
+    }
+};
+var getBuildingColor = function (clinicExtraInfo) {
+    var source = [
+        'Zelená budova',
+        'Žlutá budova',
+        'Červená budova',
+        'Fialová budova',
+    ];
+    var result = [
+        '<p style="color: green">Zelená budova</p>',
+        '<p style="color: #AA8F00">Žlutá budova</p>',
+        '<p style="color: red">Červená budova</p>',
+        '<p style="color: purple">Fialová budova</p>',
+    ];
+    var building = clinicExtraInfo;
+    for (var i = source.length - 1; i >= 0; i--) {
+        building = building && building.replace("" + source[i], "" + result[i]);
+    }
+    return building;
+};
+var getClinicLink = function (clinic, building, officeFloor) {
+    var clinicName = clinic && clinic.name && clinic.name.split(',')[0];
+    var clinicUrl = clinic && clinic.url && clinic.url.split(',')[0];
+    var officeStreet = building && building.street;
+    var officeBuilding = building && building.name;
+    return (React.createElement("div", { className: 'policlinicInfo__item--content' },
+        clinic && React.createElement("p", null,
+            React.createElement(react_apollo_1.Query, { query: GET_CONTEXT }, function (_a) {
+                var data = _a.data;
+                return (clinic.url !== undefined ?
+                    React.createElement(Link_1.default, { url: "/" + (data.languageData && data.languageData.code) + "/" + clinicUrl },
+                        "Poliklinika ",
+                        clinicName) :
+                    "Poliklinika " + clinicName);
+            })),
+        officeStreet && officeStreet.trim().length > 0 && officeStreet,
+        officeBuilding
+            && officeBuilding.trim().length > 0
+            && officeBuilding.includes('budova')
+            && React.createElement(ReactMarkdown, { skipHtml: false, escapeHtml: false, source: getBuildingColor(officeBuilding) }),
+        officeFloor && officeFloor.trim().length > 0 && React.createElement("p", null,
+            officeFloor,
+            " patro")));
+};
+var DoctorDetails = function (props) {
+    var _a = props.data, doctorName = _a.doctorName, clinic = _a.clinic, building = _a.building, officeFloor = _a.officeFloor, phone = _a.phone, thirdColImage = _a.thirdColImage, thirdColContent = _a.thirdColContent, addInfo = _a.addInfo;
+    var officePhone = phone && phone.split(',')[0] && phone.split(',')[0].trim();
+    var receptionPhone = phone && phone.split(',')[1] && phone.split(',')[1].trim();
+    var contactCenterPhone = getContactCenterPhone(clinic);
+    console.log(building);
+    return (React.createElement("section", { className: 'doctorDetails' },
+        React.createElement("div", { className: "container" },
+            doctorName && doctorName.trim().length > 0 && React.createElement("div", { className: 'doctorDetails__main' },
+                React.createElement("h3", { className: 'gradientHeading' }, doctorName)),
+            React.createElement("div", { className: "row policlinicInfo__list" },
+                (clinic
+                    || building
+                    || officeFloor)
+                    && React.createElement("div", { className: "col-12 col-lg" },
+                        React.createElement("div", { className: 'policlinicInfo__item' },
+                            React.createElement("img", { src: '/assets/medicon/images/geo.svg', alt: "address" }),
+                            getClinicLink(clinic, building, officeFloor))),
+                (officePhone && officePhone.trim().length > 0
+                    || receptionPhone && receptionPhone.trim().length > 0
+                    || contactCenterPhone && contactCenterPhone.trim().length > 0)
+                    && React.createElement("div", { className: "col-12 col-lg" },
+                        React.createElement("div", { className: 'policlinicInfo__item' },
+                            React.createElement("img", { src: '/assets/medicon/images/phone.svg', alt: "phone nubmer" }),
+                            React.createElement("ul", { className: 'policlinicInfo__item--content list-unstyled' },
+                                officePhone && officePhone.trim().length > 0 && React.createElement("li", null,
+                                    React.createElement("b", null, 'Ordinace: '),
+                                    React.createElement("a", { className: "phone", href: "callto:" + officePhone.replace(/ /g, '') }, officePhone)),
+                                receptionPhone && receptionPhone.trim().length > 0 && React.createElement("li", null,
+                                    React.createElement("b", null, 'Recepce: '),
+                                    React.createElement("a", { className: "phone", href: "callto:" + receptionPhone.replace(/ /g, '') }, receptionPhone)),
+                                contactCenterPhone && contactCenterPhone.trim().length > 0 && React.createElement("li", null,
+                                    React.createElement("b", null, 'Kontaktní centrum: '),
+                                    React.createElement("a", { className: "phone", href: "callto:" + contactCenterPhone.replace(/ /g, '') }, contactCenterPhone))))),
+                (thirdColImage || thirdColContent) && React.createElement("div", { className: "col-12 col-lg" },
+                    React.createElement("div", { className: 'policlinicInfo__item' },
+                        thirdColImage &&
+                            React.createElement(Media_1.default, { classes: 'policlinicInfo__item', type: 'image', data: thirdColImage }),
+                        thirdColContent && thirdColContent.trim().length > 0 && React.createElement("div", { className: 'policlinicInfo__item--content' },
+                            React.createElement(ReactMarkdown, { skipHtml: false, escapeHtml: false, source: thirdColContent }))))),
+            addInfo && React.createElement("div", { className: "add-info" },
+                React.createElement(ReactMarkdown, { skipHtml: false, escapeHtml: false, source: addInfo })))));
+};
+exports.default = DoctorDetails;
+var templateObject_1;
+//# sourceMappingURL=DoctorDetails.js.map
--- build/components/DoctorSchedule/DoctorSchedule.js
+++ build/components/DoctorSchedule/DoctorSchedule.js
@@ -167,6 +167,8 @@ var DoctorSchedule = function (props) {
     var redirect = function () {
         if (doctorUrl !== ''
             && doctorUrl !== props.location.pathname
+            && props.info
+            && props.info.datasources
             && props.location.pathname.includes(props.info.datasources.doctor[0])
             && doctorUrl.includes(props.info.datasources.doctor[0])) {
             props.history.replace(doctorUrl);
